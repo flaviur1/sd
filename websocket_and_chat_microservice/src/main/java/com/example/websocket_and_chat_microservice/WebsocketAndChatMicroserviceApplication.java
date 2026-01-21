20258b147c128539f0be773386cf6f22a7a160bd
Romitan Flaviu <flaviu.romitan23@gmail.com>
2026-01-21 15:29:59 +0200

    created new module for the websocket and chat microservice

---
+++ websocket_and_chat_microservice/src/main/java/com/example/websocket_and_chat_microservice/WebsocketAndChatMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocket_and_chat_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketAndChatMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketAndChatMicroserviceApplication.class, args);
+    }
+
+}
---
+++ websocket_and_chat_microservice/src/test/java/com/example/websocket_and_chat_microservice/WebsocketAndChatMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocket_and_chat_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketAndChatMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
